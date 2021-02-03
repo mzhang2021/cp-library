b4a013148edf4d655bcb1d5cbee69c08c45b901d
mzhang2021 <mzhangschool2@gmail.com>
2021-02-03 01:36:14 -0500

    debug template now properly handles nested commas

--- implementations/temp.cpp
+++ implementations/temp.cpp
@@ -2,16 +2,17 @@
 using namespace std;
 
 #ifdef LOCAL
-#define DEBUG(...) debug(#__VA_ARGS__, __VA_ARGS__);
+#define DEBUG(...) debug(#__VA_ARGS__, __VA_ARGS__)
 #else
-#define DEBUG(...) 6;
+#define DEBUG(...) 6
 #endif
 
 template<typename T, typename S> ostream& operator << (ostream &os, const pair<T, S> &p) {return os << "(" << p.first << ", " << p.second << ")";}
 template<typename C, typename T = decay<decltype(*begin(declval<C>()))>, typename enable_if<!is_same<C, string>::value>::type* = nullptr>
 ostream& operator << (ostream &os, const C &c) {bool f = true; os << "["; for (const auto &x : c) {if (!f) os << ", "; f = false; os << x;} return os << "]";}
 template<typename T> void debug(string s, T x) {cerr << s << " = " << x << "\n";}
-template<typename T, typename... Args> void debug(string s, T x, Args... args) {cerr << s.substr(0, s.find(',')) << " = " << x << " | "; debug(s.substr(s.find(',') + 2), args...);}
+template <typename T, typename... Args> void debug(string s, T x, Args... args) {for (int i=0, b=0; i<(int)s.size(); i++) if (s[i] == '(' || s[i] == '{') b++;
+else if (s[i] == ')' || s[i] == '}') b--; else if (s[i] == ',' && b == 0) {cerr << s.substr(0, i) << " = " << x << " | "; debug(s.substr(s.find_first_not_of(' ', i + 1)), args...); break;}}
 
 int main() {
     ios_base::sync_with_stdio(false);
