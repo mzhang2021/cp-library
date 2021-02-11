c08a0931c26c9ba054507f388a71230b70d95150
mzhang2021 <mzhangschool2@gmail.com>
2021-02-11 03:21:34 -0500

    changed template

--- implementations/temp.cpp
+++ implementations/temp.cpp
@@ -11,8 +11,8 @@ template<typename T, typename S> ostream& operator << (ostream &os, const pair<T
 template<typename C, typename T = decay<decltype(*begin(declval<C>()))>, typename enable_if<!is_same<C, string>::value>::type* = nullptr>
 ostream& operator << (ostream &os, const C &c) {bool f = true; os << "["; for (const auto &x : c) {if (!f) os << ", "; f = false; os << x;} return os << "]";}
 template<typename T> void debug(string s, T x) {cerr << s << " = " << x << "\n";}
-template <typename T, typename... Args> void debug(string s, T x, Args... args) {for (int i=0, b=0; i<(int)s.size(); i++) if (s[i] == '(' || s[i] == '{') b++;
-else if (s[i] == ')' || s[i] == '}') b--; else if (s[i] == ',' && b == 0) {cerr << s.substr(0, i) << " = " << x << " | "; debug(s.substr(s.find_first_not_of(' ', i + 1)), args...); break;}}
+template <typename T, typename... Args> void debug(string s, T x, Args... args) {for (int i=0, b=0; i<(int)s.size(); i++) if (s[i] == '(' || s[i] == '{') b++; else
+if (s[i] == ')' || s[i] == '}') b--; else if (s[i] == ',' && b == 0) {cerr << s.substr(0, i) << " = " << x << " | "; debug(s.substr(s.find_first_not_of(' ', i + 1)), args...); break;}}
 
 int main() {
     ios_base::sync_with_stdio(false);
