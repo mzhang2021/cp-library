d63170a5890c1051c4b3419aaffca156cd6074b0
mzhang2021 <mzhangschool2@gmail.com>
2021-02-22 01:52:51 -0500

    shortened RMQ

--- implementations/data-structures/SparseTable.h
+++ implementations/data-structures/SparseTable.h
@@ -5,34 +5,21 @@
  * Time: O(n log n) build, O(1) query
  */
 
+template<typename T>
 struct RMQ {
-    vector<int> a;
-    vector<vector<int>> spt;
+    vector<vector<T>> spt;
 
-    void init(int n, int *_a) {
-        a.resize(n);
-        spt.assign(1, vector<int>(n));
-        for (int i=0; i<n; i++) {
-            a[i] = _a[i];
-            spt[0][i] = i;
-        }
-
+    RMQ(const vector<T> &a) : spt(1, a) {
+        int n = (int) a.size();
         for (int j=1; 1<<j<=n; j++) {
             spt.emplace_back(n - (1 << j) + 1);
-            for (int i=0; i+(1<<j)<=n; i++) {
-                if (a[spt[j-1][i]] < a[spt[j-1][i+(1<<(j-1))]])
-                    spt[j][i] = spt[j-1][i];
-                else
-                    spt[j][i] = spt[j-1][i+(1<<(j-1))];
-            }
+            for (int i=0; i+(1<<j)<=n; i++)
+                spt[j][i] = min(spt[j-1][i], spt[j-1][i+(1<<(j-1))]);
         }
     }
 
-    int query(int i, int j) {
+    T query(int i, int j) {
         int k = 31 - __builtin_clz(j - i + 1);
-        if (a[spt[k][i]] < a[spt[k][j-(1<<k)+1]])
-            return spt[k][i];
-        else
-            return spt[k][j-(1<<k)+1];
+        return min(spt[k][i], spt[k][j-(1<<k)+1]);
     }
 };
