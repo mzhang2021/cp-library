fd1ee05b8366f856c47b690369c8f46de6d64839
mzhang2021 <mzhangschool2@gmail.com>
2021-02-22 02:56:32 -0500

    revamped LCA

--- implementations/graphs/LCA.h
+++ implementations/graphs/LCA.h
@@ -5,38 +5,45 @@
  * Time: O(n log n) preprocessing, O(1) query
  */
 
-#include "SparseTable.h"
+#include "../data-structures/RMQ.h"
 
-#define MAXN 100000
+struct LCA {
+    vector<int> in, path, ret, depth;
+    vector<vector<int>> adj;
+    RMQ<int> rmq;
 
-int idx, ti[MAXN], node[2*MAXN], depth[2*MAXN];
-vector<int> adj[MAXN];
-RMQ rmq;
+    LCA(int n) : in(n), depth(n), adj(n) {}
 
-void dfs(int u, int p, int d) {
-    ti[u] = idx;
-    node[idx] = u;
-    depth[idx++] = d;
-    for (int v : adj[u])
-        if (v != p) {
-            dfs(v, u, d + 1);
-            node[idx] = u;
-            depth[idx++] = d;
-        }
-}
+    void addEdge(int u, int v) {
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
 
-int lca(int u, int v) {
-    if (ti[u] > ti[v])
-        swap(u, v);
-    return node[rmq.query(ti[u], ti[v])];
-}
+    void init(int r = 0) {
+        dfs(r, -1);
+        rmq = RMQ<int>(ret);
+    }
 
-int dist(int u, int v) {
-    return depth[ti[u]] + depth[ti[v]] - 2 * depth[ti[lca(u, v)]];
-}
+    void dfs(int u, int p) {
+        in[u] = (int) path.size();
+        path.push_back(u);
+        ret.push_back(in[u]);
+        for (int v : adj[u])
+            if (v != p) {
+                depth[v] = depth[u] + 1;
+                dfs(v, u);
+                path.push_back(u);
+                ret.push_back(in[u]);
+            }
+    }
 
-void preprocess() {
-    idx = 0;
-    dfs(0, -1, 0);
-    rmq.init(idx, depth);
-}
+    int lca(int u, int v) {
+        if (in[u] > in[v])
+            swap(u, v);
+        return path[rmq.query(in[u], in[v])];
+    }
+
+    int dist(int u, int v) {
+        return depth[u] + depth[v] - 2 * depth[lca(u, v)];
+    }
+};
