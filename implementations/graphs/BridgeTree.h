59b66aaad0b3eb3ce560c27d1ba4f4dd49aa0b60
mzhang2021 <mzhangschool2@gmail.com>
2021-02-23 09:51:31 -0500

    fixed bridge tree

--- implementations/graphs/BridgeTree.h
+++ implementations/graphs/BridgeTree.h
@@ -6,35 +6,34 @@
  */
 
 struct BridgeTree {
-    int ti;
+    int n, ti;
     vector<int> num, id, stk;
     vector<vector<int>> adj, tree, comp;
 
-    BridgeTree(int n) : ti(0), num(n), id(n), adj(n) {}
+    BridgeTree(int _n) : n(_n), ti(0), num(n), id(n), adj(n) {}
 
     void addEdge(int u, int v) {
         adj[u].push_back(v);
         adj[v].push_back(u);
     }
 
-    void init(int r = 0) {
-        dfs(r, -1);
-        comp.emplace_back();
-        while (!stk.empty()) {
-            id[stk.back()] = (int) comp.size() - 1;
-            comp.back().push_back(stk.back());
-            stk.pop_back();
-        }
+    void init() {
+        for (int u=0; u<n; u++)
+            if (!num[u]) {
+                dfs(u, -1);
+                comp.emplace_back();
+                while (!stk.empty()) {
+                    id[stk.back()] = (int) comp.size() - 1;
+                    comp.back().push_back(stk.back());
+                    stk.pop_back();
+                }
+            }
         tree.resize(comp.size());
         for (auto &c : comp)
             for (int u : c)
                 for (int v : adj[u])
                     if (id[u] != id[v])
                         tree[id[u]].push_back(id[v]);
-        for (auto &c : tree) {
-            sort(c.begin(), c.end());
-            c.erase(unique(c.begin(), c.end()), c.end());
-        }
     }
 
     int dfs(int u, int p) {
@@ -47,14 +46,11 @@ struct BridgeTree {
                     low = min(low, ret);
                     if (num[u] < ret) {
                         comp.emplace_back();
-                        while (stk.back() != v) {
+                        while (comp.back().empty() || comp.back().back() != v) {
                             id[stk.back()] = (int) comp.size() - 1;
                             comp.back().push_back(stk.back());
                             stk.pop_back();
                         }
-                        id[v] = (int) comp.size() - 1;
-                        comp.back().push_back(v);
-                        stk.pop_back();
                     }
                 } else if (num[u] > num[v]) {
                     low = min(low, num[v]);
