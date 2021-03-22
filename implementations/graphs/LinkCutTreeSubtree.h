a255c7dbebf457f5df07cdf82aebc3841ce04716
mzhang2021 <mzhangschool2@gmail.com>
2021-03-22 10:36:38 -0400

    added note

--- implementations/graphs/LinkCutTreeSubtree.h
+++ implementations/graphs/LinkCutTreeSubtree.h
@@ -1,5 +1,5 @@
 /**
- * Description: Dynamic connectivity for forests, supports subtree aggregates
+ * Description: Dynamic connectivity for forests, supports subtree aggregates. Invertibility is required.
  * Source: https://codeforces.com/blog/entry/67637
  * Verification: https://judge.yosupo.jp/problem/dynamic_tree_vertex_add_subtree_sum
  * Time: O(log n)
