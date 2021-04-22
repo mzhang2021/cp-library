fb461fd6207c97d1adf706b9397943acf25e960c
mzhang2021 <mzhangschool2@gmail.com>
2021-04-22 11:37:00 -0400

    added istream operator

--- implementations/math/Modular.h
+++ implementations/math/Modular.h
@@ -22,5 +22,6 @@ struct ModInt {
     friend ModInt operator - (const ModInt &a) {return 0 - a;}
     friend ModInt power(ModInt a, long long b) {ModInt ret(1); while (b > 0) {if (b & 1) ret *= a; a *= a; b >>= 1;} return ret;}
     friend ModInt inverse(ModInt a) {return power(a, MOD - 2);}
+    friend istream& operator >> (istream &is, ModInt &m) {is >> m.v; m.v = (-MOD < m.v && m.v < MOD) ? m.v : m.v % MOD; if (m.v < 0) m.v += MOD; return is;}
     friend ostream& operator << (ostream &os, const ModInt &m) {return os << m.v;}
 };
