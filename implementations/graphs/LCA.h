53ede47014311a83edc3afc373a3c545d89d54ee
mzhang2021 <mzhangschool2@gmail.com>
2021-05-08 19:26:54 -0400

    added anc(u, v)

--- implementations/graphs/LCA.h
+++ implementations/graphs/LCA.h
@@ -8,11 +8,11 @@
 #include "../data-structures/RMQ.h"
 
 struct LCA {
-    vector<int> pos, depth, path, ret;
+    vector<int> in, out, depth, path, ret;
     vector<vector<int>> adj;
     RMQ<int> rmq;
 
-    LCA(int n) : pos(n), depth(n), adj(n), rmq({}) {}
+    LCA(int n) : in(n), out(n), depth(n), adj(n), rmq({}) {}
 
     void addEdge(int u, int v) {
         adj[u].push_back(v);
@@ -25,25 +25,30 @@ struct LCA {
     }
 
     void dfs(int u, int p) {
-        pos[u] = (int) path.size();
+        in[u] = (int) path.size();
         path.push_back(u);
-        ret.push_back(pos[u]);
+        ret.push_back(in[u]);
         for (int v : adj[u])
             if (v != p) {
                 depth[v] = depth[u] + 1;
                 dfs(v, u);
                 path.push_back(u);
-                ret.push_back(pos[u]);
+                ret.push_back(in[u]);
             }
+        out[u] = (int) path.size();
     }
 
     int lca(int u, int v) {
-        if (pos[u] > pos[v])
+        if (in[u] > in[v])
             swap(u, v);
-        return path[rmq.query(pos[u], pos[v])];
+        return path[rmq.query(in[u], in[v])];
     }
 
     int dist(int u, int v) {
         return depth[u] + depth[v] - 2 * depth[lca(u, v)];
+    }
+
+    bool anc(int u, int v) {
+        return in[u] <= in[v] && out[v] <= out[u];
     }
 };
