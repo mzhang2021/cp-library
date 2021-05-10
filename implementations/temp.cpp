3dbd37d298c56d61fc898456bbc23ef77a83cb4b
mzhang2021 <mzhangschool2@gmail.com>
2021-05-10 19:50:05 -0400

    fix space in template

--- implementations/temp.cpp
+++ implementations/temp.cpp
@@ -11,7 +11,7 @@ template<typename T, typename S> ostream& operator << (ostream &os, const pair<T
 template<typename C, typename T = decay<decltype(*begin(declval<C>()))>, typename enable_if<!is_same<C, string>::value>::type* = nullptr>
 ostream& operator << (ostream &os, const C &c) {bool f = true; os << "["; for (const auto &x : c) {if (!f) os << ", "; f = false; os << x;} return os << "]";}
 template<typename T> void debug(string s, T x) {cerr << s << " = " << x << "\n";}
-template <typename T, typename... Args> void debug(string s, T x, Args... args) {for (int i=0, b=0; i<(int)s.size(); i++) if (s[i] == '(' || s[i] == '{') b++; else
+template<typename T, typename... Args> void debug(string s, T x, Args... args) {for (int i=0, b=0; i<(int)s.size(); i++) if (s[i] == '(' || s[i] == '{') b++; else
 if (s[i] == ')' || s[i] == '}') b--; else if (s[i] == ',' && b == 0) {cerr << s.substr(0, i) << " = " << x << " | "; debug(s.substr(s.find_first_not_of(' ', i + 1)), args...); break;}}
 
 int main() {
