8c94ad5a10c2300ef62b29634a2c1efcd795de42
mzhang2021 <mzhangschool2@gmail.com>
2021-05-20 14:38:22 -0400

    changed from c-style array to vector input

--- implementations/graphs/Hungarian.h
+++ implementations/graphs/Hungarian.h
@@ -5,25 +5,21 @@
  * Time: O(n^2 * m)
  */
 
-const int MAX = 105;
-
-int n, m;
-long long adj[MAX][MAX];
-
-long long hungarian() {
-    vector<long long> u(n + 1), v(m + 1);
-    vector<int> match(m + 1);
-    for (int i=1; i<=n; i++) {
+long long hungarian(const vector<vector<long long>> &adj) {
+    int n = (int) adj.size(), m = (int) adj[0].size();
+    vector<long long> u(n), v(m);
+    vector<int> match(m);
+    for (int i=1; i<n; i++) {
         int w = 0;
         match[w] = i;
-        vector<long long> dist(m + 1, LLONG_MAX);
-        vector<int> pred(m + 1, -1);
-        vector<bool> vis(m + 1);
+        vector<long long> dist(m, LLONG_MAX);
+        vector<int> pred(m, -1);
+        vector<bool> vis(m);
         while (match[w]) {
             vis[w] = true;
             int cur = match[w], nw = 0;
             long long delta = LLONG_MAX;
-            for (int j=1; j<=m; j++)
+            for (int j=1; j<m; j++)
                 if (!vis[j]) {
                     long long edge = adj[cur][j] - u[cur] - v[j];
                     if (edge < dist[j]) {
@@ -35,7 +31,7 @@ long long hungarian() {
                         nw = j;
                     }
                 }
-            for (int j=0; j<=m; j++) {
+            for (int j=0; j<m; j++) {
                 if (vis[j]) {
                     u[match[j]] += delta;
                     v[j] -= delta;
