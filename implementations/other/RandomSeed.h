0f1c73bf4ed26f01f99c8aa71e9f06f998f598d6
mzhang2021 <maxzhang614@gmail.com>
2021-07-03 21:31:15 -0400

    remove type cast

--- implementations/other/RandomSeed.h
+++ implementations/other/RandomSeed.h
@@ -5,6 +5,6 @@
  * Time: O(1)
  */
 
-mt19937 rng((uint32_t) chrono::steady_clock::now().time_since_epoch().count());
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 
 // uniform_int_distribution<int>(a, b)(rng) generates a random integer between a and b inclusive
