fc9a031812073df30ab31079483f207b071aee2b
mzhang2021 <maxzhang614@gmail.com>
2021-10-31 14:14:45 -0400

    added generic iterative segtree

--- implementations/data-structures/SegmentTreePrimitive.h
+++ implementations/data-structures/SegmentTreePrimitive.h
@@ -8,33 +8,28 @@
 template<typename T>
 struct SegmentTree {
     int n;
-    const T id;
     vector<T> st;
 
-    T merge(const T &a, const T &b) {
-        return a + b;
-    }
-
-    SegmentTree(int _n) : n(_n), st(2 * n, id) {}
+    SegmentTree(int _n) : n(_n), st(2 * n) {}
 
     SegmentTree(const vector<T> &a) : n((int) a.size()), st(2 * n) {
         for (int i=0; i<n; i++)
             st[i+n] = a[i];
         for (int i=n-1; i>0; i--)
-            st[i] = merge(st[i<<1], st[i<<1|1]);
+            st[i] = st[i<<1] + st[i<<1|1];
     }
 
     T query(int l, int r) {
-        T ls = id, rs = id;
+        T ret = 0;
         for (l+=n, r+=n+1; l<r; l>>=1, r>>=1) {
-            if (l&1) ls = merge(ls, st[l++]);
-            if (r&1) rs = merge(st[--r], rs);
+            if (l&1) ret += st[l++];
+            if (r&1) ret += st[--r];
         }
-        return merge(ls, rs);
+        return ret;
     }
 
     void update(int p, T val) {
-        for (st[p+=n]=val, p>>=1; p>0; p>>=1)
-            st[p] = merge(st[p<<1], st[p<<1|1]);
+        for (st[p+=n]=val; p>1; p>>=1)
+            st[p>>1] = st[p] + st[p^1];
     }
 };
