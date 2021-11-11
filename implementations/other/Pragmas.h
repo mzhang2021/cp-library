d2b950851b12a1c255c7412076b1210749b268bb
mzhang2021 <maxzhang614@gmail.com>
2021-11-11 12:12:58 -0500

    added pragmas

---
+++ implementations/other/Pragmas.h
@@ -0,0 +1,10 @@
+/**
+ * Description: Copy paste for potential speed up, refer to blog for details.
+ * Source: https://codeforces.com/blog/entry/96344
+ * Verification: https://cses.fi/problemset/task/2137/
+ * Time: fast
+ */
+
+// #pragma GCC optimize("O3") - use for floating point cause Ofast uses fast-math which could mess up floating point precision
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx2")
