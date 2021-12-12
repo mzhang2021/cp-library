dabb5757e693973ec1bbfdb4cd48cd72aa6c06ed
mzhang2021 <maxzhang614@gmail.com>
2021-12-11 22:24:54 -0500

    changed order of dimensions for better cache

--- implementations/graphs/BinaryLifting.h
+++ implementations/graphs/BinaryLifting.h
@@ -10,7 +10,7 @@ struct BinaryLift {
     vector<int> depth;
     vector<vector<int>> adj, up;
 
-    BinaryLift(int _n) : n(_n), lg(__lg(n - 1) + 1), depth(n), adj(n), up(n, vector<int>(lg, -1)) {}
+    BinaryLift(int _n) : n(_n), lg(__lg(n - 1) + 1), depth(n), adj(n), up(lg, vector<int>(n, -1)) {}
 
     void addEdge(int u, int v) {
         adj[u].push_back(v);
@@ -20,22 +20,22 @@ struct BinaryLift {
     void init(int r = -1) {
         if (r == -1) {
             for (int u=0; u<n; u++)
-                if (up[u][0] == -1)
+                if (up[0][u] == -1)
                     dfs(u);
         } else {
             dfs(r);
         }
-        for (int j=1; j<lg; j++)
-            for (int i=0; i<n; i++)
-                if (up[i][j-1] != -1)
-                    up[i][j] = up[up[i][j-1]][j-1];
+        for (int i=1; i<lg; i++)
+            for (int j=0; j<n; j++)
+                if (up[i-1][j] != -1)
+                    up[i][j] = up[i-1][up[i-1][j]];
     }
 
     void dfs(int u) {
         for (int v : adj[u])
-            if (v != up[u][0]) {
+            if (v != up[0][u]) {
                 depth[v] = depth[u] + 1;
-                up[v][0] = u;
+                up[0][v] = u;
                 dfs(v);
             }
     }
@@ -43,17 +43,17 @@ struct BinaryLift {
     int lca(int u, int v) {
         if (depth[u] < depth[v])
             swap(u, v);
-        for (int j=lg-1; j>=0; j--)
-            if (depth[u] - (1 << j) >= depth[v])
-                u = up[u][j];
+        for (int i=lg-1; i>=0; i--)
+            if (depth[u] - (1 << i) >= depth[v])
+                u = up[i][u];
         if (u == v)
             return u;
-        for (int j=lg-1; j>=0; j--)
-            if (up[u][j] != up[v][j]) {
-                u = up[u][j];
-                v = up[v][j];
+        for (int i=lg-1; i>=0; i--)
+            if (up[i][u] != up[i][v]) {
+                u = up[i][u];
+                v = up[i][v];
             }
-        return up[u][0];
+        return up[0][u];
     }
 
     int dist(int u, int v) {
@@ -63,9 +63,9 @@ struct BinaryLift {
     int kthAnc(int u, int k) {
         if (k > depth[u])
             return -1;
-        for (int j=0; j<lg; j++)
-            if (k & (1 << j))
-                u = up[u][j];
+        for (int i=0; i<lg; i++)
+            if (k >> i & 1)
+                u = up[i][u];
         return u;
     }
 };
