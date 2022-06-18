5b05c176e69078b701cbd262f6e3cf8d784f9ac2
mzhang2021 <maxzhang614@gmail.com>
2022-06-17 23:57:49 -0400

    updated manacher, added notes

--- implementations/strings/Manacher.h
+++ implementations/strings/Manacher.h
@@ -1,24 +1,20 @@
 /**
- * Description: Finds the longest palindromic substring of a string. p[i] denotes the length of the longest palindrome centered at i, where if i is even, then centered on s[i / 2 - 1],
- * and if i is odd, then centered in between s[i / 2 - 1] and s[i / 2].
+ * Description: Finds the longest palindromic substring of a string. p[i] denotes the length of the longest palindrome centered at i,
+ * where if i is even, then centered on s[i / 2 - 1], and if i is odd, then centered in between s[i / 2 - 1] and s[i / 2].
+ * To recover the actual endpoints of each palindrome, use l = (i - p[i] - 1) / 2 and r = (i + p[i] - 3) / 2.
  * Source: https://www.youtube.com/watch?v=nbTSfrEfo6M
  * Verification: https://leetcode.com/problems/longest-palindromic-substring/
  * Time: O(n)
  */
 
-#define MAXN 100000
-
-int p[2*MAXN+3];
-
-int manacher(const string &s) {
-    memset(p, 0, sizeof(p));
+vector<int> manacher(const string &s) {
     string t = "$";
     for (char c : s)
         t += '#', t += c;
     t += "#@";
-
-    int c = 0, r = 0;
-    for (int i=1; i<t.length()-1; i++) {
+    int n = (int) t.size(), c = 0, r = 0;
+    vector<int> p(n - 1);
+    for (int i=1; i<n-1; i++) {
         int mirr = 2 * c - i;
         if (i < r)
             p[i] = min(p[mirr], r - i);
@@ -29,6 +25,5 @@ int manacher(const string &s) {
             r = i + p[i];
         }
     }
-
-    return *max_element(p, p + t.length());
+    return p;
 }
