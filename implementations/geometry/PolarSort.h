2437abb11079d954d75d5818a3b780d08568f6bf
mzhang2021 <maxzhang614@gmail.com>
2023-01-18 01:11:26 -0500

    added polar sort

---
+++ implementations/geometry/PolarSort.h
@@ -0,0 +1,18 @@
+/**
+ * Description: Sorts vectors radially based on angles [0, 2pi). For parallel vectors, order is arbitrary. Inclusion of (0, 0) is undefined.
+ * Source: https://codeforces.com/blog/entry/72815
+ * Verification: https://open.kattis.com/problems/leylines
+ * Time: O(1)
+ */
+
+#include "Point.h"
+
+template<typename T>
+bool half(const Point<T> &p) {
+    return p.y < 0 || (p.y == 0 && p.x < 0);
+}
+
+template<typename T>
+bool cmp(const Point<T> &p, const Point<T> &q) {
+    return half(p) == half(q) ? cross(p, q) > 0 : half(p) < half(q);
+}
