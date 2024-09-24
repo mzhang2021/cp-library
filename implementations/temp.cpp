a2f2d26d2cd8161f151c29ba6bf4957fa82bcea1
mzhang2021 <maxzhang614@gmail.com>
2024-09-23 23:06:25 -0400

    update template

--- implementations/temp.cpp
+++ implementations/temp.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// clang-format off
 #ifdef LOCAL
 #define DEBUG(...) debug(#__VA_ARGS__, __VA_ARGS__)
 #else
@@ -13,10 +14,21 @@ ostream& operator << (ostream &os, const C &c) {bool f = true; os << "["; for (c
 template<typename T> void debug(string s, T x) {cerr << "\033[1;35m" << s << "\033[0;32m = \033[33m" << x << "\033[0m\n";}
 template<typename T, typename... Args> void debug(string s, T x, Args... args) {for (int i=0, b=0; i<(int)s.size(); i++) if (s[i] == '(' || s[i] == '{') b++; else
 if (s[i] == ')' || s[i] == '}') b--; else if (s[i] == ',' && b == 0) {cerr << "\033[1;35m" << s.substr(0, i) << "\033[0;32m = \033[33m" << x << "\033[31m | "; debug(s.substr(s.find_first_not_of(' ', i + 1)), args...); break;}}
+// clang-format on
+
+void test_case() {
+  // solve here
+}
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
 
-    return 0;
+  int t = 1;
+  cin >> t;
+  while (t--) {
+    test_case();
+  }
+
+  return 0;
 }
